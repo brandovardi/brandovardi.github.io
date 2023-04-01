f099add02d021d9400afca43c24904bba76703df
Pietro Brandovardi <65778541+brandovardi@users.noreply.github.com>
2023-04-01 16:14:14 +0200

    Update script.js

--- Calcolatrice/script/script.js
+++ Calcolatrice/script/script.js
@@ -1,47 +1,47 @@
 // classe calcolatrice
-class Calcolatrice{
+class Calcolatrice {
     // unico costruttore di default
     constructor() {
-        this.parziale = "";
-        this.numCorrente = "";
-        this.segno = ' ';
+        this.secondo = "";
+        this.numCorr = "";
+        this.operazione = ' ';
     }
     // metodo per aggiornare il numero inserito dall'utente sia nel display che nell'attributo
-    aggiungiValore(n) {
-        this.numCorrente += n;
-        document.getElementById('text').value = this.numCorrente;
+    numero(n) {
+        this.numCorr += n;
+        document.getElementById('text').value = this.numCorr;
     }
     // metodo per aggiornare il segno, ovvero l'operazione da effettuare
-    segnoCliccato(s) {
-        this.segno = s;
-        this.parziale = this.numCorrente;
-        this.numCorrente = "";
+    segno(s) {
+        this.operazione = s;
+        this.secondo = this.numCorr;
+        this.numCorr = "";
         document.getElementById('text').value = "";
     }
     // metodo per calcolare il risultato dell'operazione selezionata
     calcola(){
         let risultato = 0;
-        if(this.segno == '+') {
-            risultato = parseInt(this.parziale) + parseInt(this.numCorrente);
+        if(this.operazione == '+') {
+            risultato = parseInt(this.secondo) + parseInt(this.numCorr);
         }
-        else if(this.segno == '-') {
-            risultato = parseInt(this.parziale) - parseInt(this.numCorrente);
+        else if(this.operazione == '-') {
+            risultato = parseInt(this.secondo) - parseInt(this.numCorr);
         }
-        else if(this.segno == 'x') {
-            risultato = parseInt(this.parziale) * parseInt(this.numCorrente);
+        else if(this.operazione == 'x') {
+            risultato = parseInt(this.secondo) * parseInt(this.numCorr);
         }
-        else if(this.segno == '÷') {
-            risultato = parseInt(this.parziale) / parseInt(this.numCorrente);
+        else if(this.operazione == '÷') {
+            risultato = parseInt(this.secondo) / parseInt(this.numCorr);
         }
         document.getElementById('text').value = risultato;
-        this.parziale = "";
-        this.numCorrente = risultato;
+        this.secondo = "";
+        this.numCorr = risultato;
     }
     // metodo per pulire lo schermo
     clear() {
-        this.parziale = "";
-        this.numCorrente = "";
-        this.segno = "";
+        this.secondo = "";
+        this.numCorr = "";
+        this.operazione = "";
         document.getElementById('text').value = "";
     }
 
