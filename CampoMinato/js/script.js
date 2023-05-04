15548eeac1a027068e424de9243155e64811f940
Pietro Brandovardi <65778541+brandovardi@users.noreply.github.com>
2023-05-04 09:56:58 +0200

    Update script.js

--- CampoMinato/js/script.js
+++ CampoMinato/js/script.js
@@ -3,9 +3,22 @@ $(document).ready(function () {
 
     $('#select').change(function () {
         let x = $('#select');
-        
-        const campo = new Campo(x[0].value, x[0].value);
+        x = x[0].value;
+        let campo;
+        if (x == "1") {
+            campo = new Campo(10, 10);
+        }
+        else if (x == "2") {
+            campo = new Campo(20, 55);
+        }
+        else if (x == "3") {
+            campo = new Campo(30, 110);
+        }
+        else if (x == "4") {
+            campo = new Campo(25, 150);
+        }
 
+        campo.clear();
         campo.generaCampo();
         
         $('#first').hide();
