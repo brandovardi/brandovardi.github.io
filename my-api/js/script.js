144100ee384d1498f3cac2f25a978bac0b6e1bd5
brandovardi <brandovardipietro@outlook.it>
2025-02-12 12:55:19 +0100

    .

--- my-api/js/script.js
+++ my-api/js/script.js
@@ -1,18 +1,29 @@
-(function() {
-    const originalFetch = window.fetch;
-    window.fetch = async function(resource, options) {
-        if (options && options.method === "GET") {
-            console.log("Intercepted fetch POST request to:", resource);
-            console.log("Request body:", options.body);
 
-            // take the url parameter
-            let url = new URL(resource);
-            // run the python script passing the url parameter
-            let git_link = "http://brandovardi.github.io/my-api/py/x.py ";
-            let response = await fetch(git_link + url.searchParams.get("url"));
-            let data = await response.json();
-            console.log(data);
-        }
-        return originalFetch.apply(this, arguments);
-    };
-})();
+$(document).ready(function () {
+    console.log("Script loaded!");
+    
+    (function () {
+        console.log("Overriding fetch() function");
+
+        const originalFetch = window.fetch;
+        console.log("Original fetch function:", originalFetch);
+
+        window.fetch = async function (resource, options) {
+            console.log("Intercepted fetch() call");
+
+            if (options && options.method === "GET") {
+                console.log("Intercepted fetch POST request to:", resource);
+                console.log("Request body:", options.body);
+
+                // take the url parameter
+                let url = new URL(resource);
+                // run the python script passing the url parameter
+                let git_link = "http://brandovardi.github.io/my-api/py/x.py ";
+                let response = await fetch(git_link + url.searchParams.get("url"));
+                let data = await response.json();
+                console.log(data);
+            }
+            return originalFetch.apply(this, arguments);
+        };
+    })();
+});
